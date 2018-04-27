5100685c56144c24f67ffcda4efe0134317e82c6
darecho <35001232+darecho@users.noreply.github.com>
2018-04-27 09:35:16 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+	x = 1;
+	y = 2;
+	cout << x + y << endl;
+	cin >> x;
+    return 0;
+}
+
